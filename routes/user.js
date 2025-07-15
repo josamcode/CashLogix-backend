dc65097e2b39e391dc25842c247f7c91b53d1de3
josamcode <gergessamuel100@gmail.com>
2025-07-15 19:50:15 +0300

    fix frontend design

--- routes/user.js
+++ routes/user.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const {
   createUser,
   getUser,
-  updateUser,
-  deleteUser,
+  // updateUser,
+  // deleteUser,
   loginUser,
 } = require("../controllers/user");
 
@@ -13,8 +13,8 @@ const { verifyToken } = require("../middlewares/auth");
 // POST /createUser
 router.post("/auth/register", createUser);
 router.get("/getUser", verifyToken, getUser);
-router.put("/updateUser/:id", updateUser);
-router.delete("/deleteUser/:id", deleteUser);
+// router.put("/updateUser/:id", updateUser);
+// router.delete("/deleteUser/:id", deleteUser);
 router.post("/auth/login", loginUser);
 
 module.exports = router;
